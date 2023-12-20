c6cce5d3052a58bd7e6d451104fd31bbc6a5e1fb
Marvin <marvin.brouwer@live.nl>
2023-12-20 15:10:31 +0100

    Added basic cms component

---
+++ src/EpiUiExtensions.CmsComponent/PropertyBase/PropertyJsonBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using EPiServer.Core;
+using EPiServer.Framework.Serialization;
+using EPiServer.Logging;
+using EPiServer.ServiceLocation;
+
+namespace EpiUiExtensions.CmsComponent.PropertyBase;
+
+/// <summary>
+/// Property base class to store any kind of data into an EpiServer property using JSON
+/// </summary>
+public abstract class PropertyJsonBase<TModel> : PropertyLongString
+    where TModel : class, new()
+{
+    private readonly IObjectSerializer _objectSerializer;
+    private readonly ILogger _logger;
+
+    /// <inheritdoc />
+    protected PropertyJsonBase()
+    {
+        _objectSerializer = ServiceLocator.Current.GetInstance<IObjectSerializerFactory>()
+            .GetSerializer("application/json");
+        _logger = LogManager.GetLogger(GetType());
+    }
+
+    public override Type PropertyValueType => typeof(TModel);
+    public override object SaveData(PropertyDataCollection properties) => LongString;
+    public override object? Value { get => GetValue(); set => SetValue(value); }
+    public TModel? ModelValue { get => GetValue(); set => SetValue(value); }
+
+    private void SetValue(object? value)
+    {
+        if (value is null) LongString = null;
+        if (value is string stringValue) LongString = stringValue;
+        if (value is not TModel) return;
+
+        try
+        {
+            LongString = _objectSerializer.Serialize(value);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"Failed to serialize {typeof(TModel)}", ex);
+        }
+    }
+
+    private TModel? GetValue()
+    {
+        if (string.IsNullOrEmpty(LongString)) return default;
+
+        try
+        {
+            return _objectSerializer.Deserialize<TModel>(LongString);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"Failed to deserialize {typeof(TModel)}", ex);
+            return default;
+        }
+    }
+}
---
+++ src/EpiUiExtensions.CmsComponent/ServerModule/EmbeddedComponentController.cs
@@ -0,0 +1,64 @@
+﻿using EPiServer.Web;
+
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Web.Http;
+
+namespace EpiUiExtensions.CmsComponent.ServerModule;
+
+/// <summary>
+/// API controller to fake EPiServer into thinking we've included the component in the shell folder
+/// </summary>
+[RoutePrefix("EPiServer/Shell/{version}/ClientResources/{moduleNameSpace}/{componentName}/")]
+public abstract class EmbeddedComponentController : ApiController
+{
+    private readonly IMimeTypeResolver _mimeTypeResolver;
+    private readonly Assembly _targetAssembly;
+
+    /// <inheritdoc cref="EmbeddedComponentController"/>
+    protected EmbeddedComponentController(IMimeTypeResolver mimeTypeResolver)
+    {
+        _mimeTypeResolver = mimeTypeResolver;
+        // Gets the assembly of the implementing type
+        _targetAssembly = GetType().Assembly;
+    }
+
+    /// <summary>
+    /// Pretend to be component/*
+    /// </summary>
+    [HttpGet, Route("{*path}")]
+    public virtual HttpResponseMessage Index(string version, string path)
+    {
+        // We use the version attribute to make it dynamic
+        _ = version;
+
+        var mimeType = _mimeTypeResolver.GetMimeMapping(path);
+
+        // Don't dispose, that breaks the controller logic
+        var fileStream = GetResourceFile(path);
+        fileStream.Seek(0, SeekOrigin.Begin);
+
+        return new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StreamContent(fileStream)
+            {
+                Headers =
+                {
+                    ContentType = new MediaTypeHeaderValue(mimeType ?? "application/octet-stream"),
+                    ContentEncoding = { Encoding.UTF8.WebName }
+                }
+            }
+        };
+    }
+
+    private Stream GetResourceFile(string fileName)
+    {
+        var createDbScriptAssemblyPath = _targetAssembly.GetManifestResourceNames().First(name => name.EndsWith(fileName));
+        return _targetAssembly.GetManifestResourceStream(createDbScriptAssemblyPath)!;
+    }
+}
